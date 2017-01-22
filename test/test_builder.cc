10d3bbb764d453c83e1528f6aba4ad57b0f7c98f
Loic Dachary <loic@dachary.org>
2017-01-22 11:07:02 +0100

    Add test stub for crush_make_bucket
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- test/test_builder.cc
+++ test/test_builder.cc
@@ -5,7 +5,24 @@ extern "C" {
 }
 
 TEST(builder, crush_create) {
-  crush_map *c = crush_create();
-  EXPECT_EQ(NULL, c->rules);
-  free(c);
+  crush_map *m = crush_create();
+  EXPECT_EQ(NULL, m->rules);
+  crush_destroy(m);
+}
+
+TEST(builder, crush_make_bucket) {
+  crush_map *m = crush_create();
+  const int type = 1;
+  const int size = 1;
+  const int hash = 3;
+  int weights[1] = { 1 };
+  int items[1] = { 1 };
+  crush_bucket *b = crush_make_bucket(m, CRUSH_BUCKET_UNIFORM, hash, type,
+                                      size, items, weights);
+  EXPECT_EQ(CRUSH_BUCKET_UNIFORM, b->alg);
+  crush_destroy(m);
+}
+
+TEST(builder, crush_multiplication_is_unsafe) {
+  ASSERT_TRUE(crush_multiplication_is_unsafe(1, 0));
 }
