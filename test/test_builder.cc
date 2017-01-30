59da8a2c62328202857e8e0297bd10b1d7f91ab1
Loic Dachary <loic@dachary.org>
2017-01-30 15:31:37 +0100

    add unit tests for crush_add_bucket
    closes #12
    
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- test/test_builder.cc
+++ test/test_builder.cc
@@ -29,6 +29,27 @@ TEST(builder, crush_make_bucket) {
   crush_destroy(m);
 }
 
+TEST(builder, crush_add_bucket) {
+  crush_map *m = crush_create();
+  const int type = 1;
+  const int size = 1;
+  const int hash = 3;
+  crush_bucket *b = crush_make_bucket(m, CRUSH_BUCKET_STRAW2, hash, type,
+                                      0, NULL, NULL);
+  int bucketno = 0;
+  ASSERT_EQ(0, crush_add_bucket(m, 0, b, &bucketno));
+  ASSERT_EQ(-1, bucketno);
+  ASSERT_EQ(-EEXIST, crush_add_bucket(m, bucketno, b, &bucketno));
+
+  for(int i = 0; i < 1024; i++) {
+    crush_bucket *b = crush_make_bucket(m, CRUSH_BUCKET_STRAW2, hash, type,
+                                        0, NULL, NULL);
+    ASSERT_EQ(0, m->max_buckets % 2);
+    ASSERT_EQ(0, crush_add_bucket(m, 0, b, &bucketno));
+  }
+  crush_destroy(m);
+}
+
 TEST(builder, crush_multiplication_is_unsafe) {
   ASSERT_TRUE(crush_multiplication_is_unsafe(1, 0));
 }
